f997fcbd6d0cf0f0f4b190832aa1331833f2dbef
Parker Moore <parkrmoore@gmail.com>
2012-07-13 10:55:00 +0100

    Added schema & migrated comments table

--- db/migrate/20120713095036_create_comments.rb
+++ db/migrate/20120713095036_create_comments.rb
@@ -1,10 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.integer :id
-      t.text :content
-      t.integer :user_id
-      t.integer :item_id
+      t.integer :id, :null => false
+      t.text :content, :null => false
+      t.integer :user_id, :null => false
+      t.integer :item_id, :null => false
 
       t.timestamps
     end
