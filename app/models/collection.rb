425e224e78c2713b1383c6cf651449d94b1faabc
Parker Moore <parkrmoore@gmail.com>
2012-07-13 13:58:48 +0100

    Collection: user_id added, formatted_location added, validating length of contact_person

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,17 +1,31 @@
 class Collection < ActiveRecord::Base
-  attr_accessible :city, :collection_id, :contact_person, :country, :is_event, :name, :state, :venue, :when
+  attr_accessible :city, :collection_id, :contact_person, :country, :is_event, :name, :state, :venue, :when, :user_id
   
+  belongs_to :collection
   has_many :collections
   has_many :albums
   
   acts_as_tree
   
   validates_presence_of :name
+  validates_length_of :contact_person, :maximum => 255, :message => "255 characters max, please."
   validates_presence_of :when, :city, :state, :country, :if => Proc.new { |collection| collection.is_event }
   
   def self.collections_for_select
     collections = Collection.all
     collections.unshift Collection.new({:id => 0, :name => "None"})
   end
+  
+  def formatted_location
+    location = [self.city, self.state, self.country]
+    location.reject! { |piece| piece.nil? || piece.blank? }
+    municipalities = location.join(", ")
+    
+    municipalities = "At #{self.venue} in #{municipalities}" unless self.venue || self.venue.nil? || self.venue.blank?
+    municipalities = "In #{municipalities}" if (self.venue.nil? || self.venue.blank?) && !municipalities.empty?
+    
+    municipalities
+  end
+  
   
 end
