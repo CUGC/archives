d8fd62e6483e26bd965cd15942299be3d683cffb
Parker Moore <parkrmoore@gmail.com>
2012-07-13 13:59:11 +0100

    Added user_id to albums and collections

--- db/migrate/20120713120518_add_user_id_to_collections_and_albums.rb
+++ db/migrate/20120713120518_add_user_id_to_collections_and_albums.rb
@@ -1,4 +1,12 @@
 class AddUserIdToCollectionsAndAlbums < ActiveRecord::Migration
-  def change
+  def up
+    add_column :collections, :user_id, :integer, :null => false
+    add_column :albums, :user_id, :integer, :null => false
   end
+  
+  def down
+    remove_column :collections, :user_id
+    remove_column :albums, :user_id
+  end
+  
 end
