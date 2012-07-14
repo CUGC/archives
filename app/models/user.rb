014c621f04d9ec0d32641b5298c65ba0b4ea1b5d
Parker Moore <parkrmoore@gmail.com>
2012-07-14 12:43:02 +0100

    Added User relationships and roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :role
+  attr_accessible :email, :first_name, :last_name, :role, :password, :password_confirmation
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -7,5 +7,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable, :omniauthable
+   
+  has_many :items
+  has_many :albums
+  has_many :collections
+  
+  ROLES = %w[admin moderator member]
   
 end
